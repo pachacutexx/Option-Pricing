b44442c6e20f3bef63aedd869f97c36c2bd0811d
pachacutexx <152634036+pachacutexx@users.noreply.github.com>
2023-12-05 15:37:54 +0100

    Update option_pricer.py

--- src/option_pricer.py
+++ src/option_pricer.py
@@ -38,44 +38,71 @@ class EuropeanOption:
 
         return option_values[0]
 
+    def _generate_stock_prices(self, steps, dt):
+        up = np.exp(self.sigma * np.sqrt(2 * dt))
+        down = 1 / up
+
+        vec_u = np.cumprod(up * np.ones(steps))
+        vec_d = np.cumprod(down * np.ones(steps))
+
+        stock_prices = np.concatenate((vec_d[::-1], [1.0], vec_u)) * self.S0
+        return stock_prices
+
     def trinomial_price(self):
         dt = self.T / self.steps
-        u = np.exp(self.sigma * np.sqrt(2 * dt))
-        d = 1 / u
-        m = 1
-        pu = ((np.exp(self.r * dt / 2) - np.exp(-self.sigma * np.sqrt(dt / 2))) /
-              (np.exp(self.sigma * np.sqrt(dt / 2)) - np.exp(-self.sigma * np.sqrt(dt / 2))))**2
-        pd = ((np.exp(self.sigma * np.sqrt(dt / 2)) - np.exp(self.r * dt / 2)) /
-              (np.exp(self.sigma * np.sqrt(dt / 2)) - np.exp(-self.sigma * np.sqrt(dt / 2))))**2
+        discount = np.exp(-self.r * dt)
+
+        # Calculate risk-neutral probabilities
+        pu = (
+            (np.exp(self.r * dt / 2) - np.exp(-self.sigma * np.sqrt(dt / 2))) /
+            (np.exp(self.sigma * np.sqrt(dt / 2)) - np.exp(-self.sigma * np.sqrt(dt / 2)))
+        ) ** 2
+        pd = (
+            (np.exp(self.sigma * np.sqrt(dt / 2)) - np.exp(self.r * dt / 2)) /
+            (np.exp(self.sigma * np.sqrt(dt / 2)) - np.exp(-self.sigma * np.sqrt(dt / 2)))
+        ) ** 2
         pm = 1 - pu - pd
 
-        # Initialize asset prices at maturity
-        ST = np.zeros(2 * self.steps + 1)
-        ST[0] = self.S0 * d**self.steps
+        # Generate stock prices at maturity
+        s = self._generate_stock_prices(self.steps, dt)
 
-        for i in range(1, 2 * self.steps + 1):
-            ST[i] = ST[i - 1] * u if i % 2 == 1 else ST[i - 1] * m
+        # Define Payoff
+        final_payoff = np.maximum(s - self.K, 0) if self.is_call else np.maximum(self.K - s, 0)
+        nxt_vec_prices = final_payoff
 
-        # Calculate option values at maturity
-        option_values = np.maximum(ST - self.K, 0) if self.is_call else np.maximum(self.K - ST, 0)
+        # Iterations for the calculation of payoffs
+        for i in range(1, self.steps + 1):
+            vec_stock = self._generate_stock_prices(self.steps - i, dt)
+            expectation = np.zeros(len(vec_stock))
 
-        # Step backwards through the tree
-        for i in range(self.steps, 0, -1):
-            for j in range(2 * i - 1):
-                option_values[j] = (pu * option_values[j + 2] + pm * option_values[j + 1] + pd * option_values[j]) * np.exp(-self.r * dt)
+            for j in range(len(expectation)):
+                tmp = nxt_vec_prices[j] * pd
+                tmp += nxt_vec_prices[j + 1] * pm
+                tmp += nxt_vec_prices[j + 2] * pu
 
-        return option_values[0]
+                expectation[j] = tmp
+            # Discount option payoff
+            nxt_vec_prices = discount * expectation
+
+        # Return the expected discounted value of the option at t=0
+        return nxt_vec_prices[0]
 
     def black_scholes_price(self):
-        d1 = (np.log(self.S0 / self.K) + (self.r + 0.5 * self.sigma**2) * self.T) / (self.sigma * np.sqrt(self.T))
-        d2 = d1 - self.sigma * np.sqrt(self.T)
+    # Calculate d1 using the Black-Scholes formula components.
+    d1 = (np.log(self.S0 / self.K) + (self.r + 0.5 * self.sigma**2) * self.T) / (self.sigma * np.sqrt(self.T))
 
-        if self.is_call:
-            price = (self.S0 * norm.cdf(d1) - self.K * np.exp(-self.r * self.T) * norm.cdf(d2))
-        else:
-            price = (self.K * np.exp(-self.r * self.T) * norm.cdf(-d2) - self.S0 * norm.cdf(-d1))
+    # Calculate d2, which is derived from d1 but adjusted by the volatility over time.
+    d2 = d1 - self.sigma * np.sqrt(self.T)
 
-        return price
+    # Calculate the option price based on whether it is a call or a put option.
+    if self.is_call:
+        price = (self.S0 * norm.cdf(d1) - self.K * np.exp(-self.r * self.T) * norm.cdf(d2))
+    else:
+        price = (self.K * np.exp(-self.r * self.T) * norm.cdf(-d2) - self.S0 * norm.cdf(-d1))
+
+    # Return the calculated price.
+    return price
+
 
     def price(self, method='binomial'):
         if method == 'binomial':
